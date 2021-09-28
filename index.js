014875b80ccc83d4802738e94ae66d46ab711ae3
devendra-lab <83896078+devendra-lab@users.noreply.github.com>
2021-09-28 08:32:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// JS for Preloader
+let body = document.body
+let preload = document.querySelector('#preloader')
+body.onload = () => {
+    preload.style.display = "none"
+}
+// Jquery for Nav-bar
+$(document).ready(() => {
+    $('#nav').click(() => {
+        $('.nav-bar').toggleClass('active')
+        $('#nav').toggleClass('fa-times')
+    })
+    $(window).scroll(() => {
+        $('.nav-bar').removeClass('active')
+        $('#nav').removeClass('fa-times')
+    })
+})
